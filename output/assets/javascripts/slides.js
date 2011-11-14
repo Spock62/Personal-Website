dbc87b75e305a1a4d389fd3b5a04835aa297b9c8
David <david@hermes.(none)>
2011-11-13 20:08:25 -0800

    Working on slideshow and language game

--- output/assets/javascripts/slides.js
+++ output/assets/javascripts/slides.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-  var current_slide = 0;
+  var slide_url = $(location).attr('href').split("#");
+  if (slide_url.length > 1) {
+    var current_slide = (parseInt(slide_url[1].replace("slide", ""))-1);
+  }
+  else {
+    var current_slide = 0;
+  }
   $(this).keydown(function(e){
     var slides = $(".slide");
     //Handle right press
@@ -7,6 +13,7 @@ $(document).ready(function() {
       if (current_slide !== 0) {
         current_slide--;
         $.scrollTo(slides[current_slide], 800);
+        window.history.pushState("slide" + (current_slide+1), document.title, "#slide" + (current_slide+1));
       }
     }
     //Handle right press
@@ -14,6 +21,7 @@ $(document).ready(function() {
       if (current_slide <= slides.length) {
         current_slide++;
         $.scrollTo(slides[current_slide], 800);
+        window.history.pushState("slide" + (current_slide+1), document.title, "#slide" + (current_slide+1));
       }
     }
   });
